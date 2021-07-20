e3fd4b164652601cb6b59b54648bd82c80eae72e
shigure3011 <thanhtung30112001@gmail.com>
2021-07-20 17:53:48 +0000

    Add converter #3

--- converter.py
+++ converter.py
@@ -5,11 +5,9 @@ import numpy as np
 import skimage.transform as skTrans
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
-
 
 # Config logging
-logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
 
 
 # Add parser
@@ -46,20 +44,24 @@ def convert_to_pkl(img_dir: str, output_dir: str, img_shape: tuple, output_shape
     
     assert all(i > 0 for i in output_shape), "Output shape must have positive values."
 
-    cases = get_files(img_dir)
+    cases = get_filenames(img_dir)
     assert len(cases) > 0, "{} is empty.".format(img_dir)
 
-    for case_dir in tqdm(cases):
+    n = len(cases)
+    for it in range(n):
+        case_dir = cases[it]
         images = load_case(case_dir, img_shape, output_shape)
         assert images.shape == (4, C, H, W),\
             "Case {0} has incorrect shape {1}, which is different from {2}"\
             .format(case_dir, images.shape, (4, C, H, W))
+
         save_case(images, case_dir, output_dir)
+        logging.info("[{1}/{2}] Saved {0}".format(output_dir, it + 1, n))
 
     logging.info('Preprocessing completed.')
 
 
-def get_files(path: str) -> list:
+def get_filenames(path: str) -> list:
     files = glob.glob(os.path.join(path, '*'))
     return sorted(files)
 
@@ -76,24 +78,22 @@ def load_case(case_dir: str, img_shape: tuple, output_shape: tuple) -> np.ndarra
         assert modal_img.shape == (1, C, H, W),\
             "Modal {0} has incorrect shape {1}, which is different from {2}"\
             .format(modal_path, modal_img.shape, (1, C, H, W))
-        np.concatenate((images, modal_img), axis = 0)
+        images = np.concatenate((images, modal_img), axis = 0)
 
     return images
 
 
 def save_case(images: np.ndarray, case_dir: str, output_dir: str) -> None:
-    output_name = os.path.dirname(case_dir) + '.pkl.bz2'
-    output_path = os.path.join(output_dir, out_name)
-
-    logging.debug("Saving to {}".format(output_path))
+    output_name = extract_folder_name(case_dir) + '.pkl.bz2'
+    output_path = os.path.join(output_dir, output_name)
     pickle.dump(images, bz2.open(output_path, 'wb'))
 
 
 def load_modal(path: str, img_shape: tuple, output_shape: tuple) -> np.ndarray:
     C, H, W = img_shape
 
-    if not os.path.exists(path) or len(get_files(path)) == 0:
-        logging.debug('{} does not contain images.'.format(path))
+    if not os.path.exists(path) or len(get_filenames(path)) == 0:
+        logging.info('{} does not contain images, paddad by zeros'.format(path))
         return np.zeros(output_shape, dtype=np.uint8)[:,]
 
     images = load_images(path, img_shape)
@@ -101,9 +101,8 @@ def load_modal(path: str, img_shape: tuple, output_shape: tuple) -> np.ndarray:
     assert len(images.shape) == 3,\
         "Images {} must have shape of length 3".format(path)
 
-    logging.debug("Images {0} has shape {1}".format(path, images.shape))
-    skTrans.resize(images, output_shape, order=1, preserve_range=True)
-
+    images = skTrans.resize(images, output_shape, order=1, preserve_range=True)
+    logging.info('Loaded {}'.format(path))
     return images[None, :]
 
 
@@ -121,10 +120,14 @@ def load_images(path: str, img_shape: tuple) -> np.ndarray:
             "{0} has size {1}, which is different from {2}".format(img_path, img.shape, (W, H))
 
         img = transform(img)
-        np.concatenate((imgs, img), axis = 0)
+        imgs = np.concatenate((imgs, img), axis = 0)
         logging.debug('Loaded {0} - Current shape {1}'.format(img_path, imgs.shape))
 
     return imgs
+
+
+def extract_folder_name(path: str)-> str:
+    return path[(path.rfind('/') + 1):]
 
 
 def get_index(path):
